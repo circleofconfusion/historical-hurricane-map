ba7d8b82e2ca8bf4cdb796999e60a1e2f8fc2f7a
Shane Knudsen <shaneknu@gmail.com>
2020-04-10 23:48:13 -0400

    Basic ZephJS components created and imported into index.

---
+++ sidebar/app-sidebar.js
@@ -0,0 +1,16 @@
+
+/*
+	Generated ZephS component: app-sidebar
+
+	ZephJS is copyright 2019, The Awesome Engineering Company
+	and is released under the MIT licesne.
+ */
+
+import {ZephComponents,html,css} from "./zeph.min.js";
+
+ZephComponents.define("app-sidebar",()=>{
+	html("./app-sidebar.html");
+	css("./app-sidebar.css");
+
+	// Place your compnent defintion calls here. See the ZephJS documentation for more information.
+});
