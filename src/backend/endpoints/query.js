7eb37094cb901853c967f162ee11bd1eaf0609e6
Shane Knudsen <shaneknu@gmail.com>
2020-04-19 12:50:18 -0400

    Setup .eslintrc.json files in backend/frontend directories b/c different rules about globals.

--- src/backend/endpoints/query.js
+++ src/backend/endpoints/query.js
@@ -6,9 +6,10 @@ const util = require('util');
 
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const query = mongoose.model('Hurricane', Hurricane).find(
+mongoose.model('Hurricane', Hurricane).find(
   { 'properties.year': 2018 },
   function(err, result) {
     console.log(util.inspect(result, {  depth: null }));
     mongoose.connection.close();
-  });
+  }
+);
