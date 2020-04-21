bbd2c5bf23ecce925cb076b0f8f893cd4498bd65
Shane Knudsen <shaneknu@gmail.com>
2020-04-21 12:57:05 -0400

    WIP name search

--- src/backend/query.js
+++ src/backend/query.js
@@ -5,11 +5,14 @@ const mongoose = require('mongoose');
 const { Hurricane } = require('./schemas/Hurricane');
 const util = require('util');
 
-module.exports = { queryYear };
+module.exports = { 
+  queryByYear,
+  queryByName
+};
 
-async function queryYear(event) {
-  console.log(util.inspect(event));
-  mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function queryByYear(event) {
 
   const result = await mongoose.model('Hurricane', Hurricane).find({ 'properties.year': event.pathParameters.year }).exec();
 
@@ -20,4 +23,15 @@ async function queryYear(event) {
     body: JSON.stringify(result)
   };
 
+}
+
+async function queryByName(event) {
+  const result = await mongoose.model('Hurricane', Hurricane).find({ 'properties.name': `'${event.pathParameters.name.toUpperCase()}'` }).exec();
+  
+  mongoose.connection.close();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result)
+  };
 }
