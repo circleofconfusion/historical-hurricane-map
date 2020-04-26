daff36d9913d5d0b9c03e7082ab6e609e3eff0ad
Shane Knudsen <shaneknu@gmail.com>
2020-04-26 10:11:44 -0400

    Setup dist directory. Installed and configured rollup-plugin-copy

--- src/frontend/rollup.config.js
+++ src/frontend/rollup.config.js
@@ -1,14 +1,25 @@
 import resolve from '@rollup/plugin-node-resolve';
 import zephjsInline from '@rollup/plugin-zephjs-inline';
+import copy from 'rollup-plugin-copy';
 
 export default [
   {
     input: 'index.js',
     output: {
-      file: 'rolled-up-bundle.js',
+      file: '../../dist/bundle.js',
       format: 'es',
       name: 'bundle'
     },
-    plugins: [ resolve(), zephjsInline({ quiet: false }) ]
+    plugins: [
+      resolve(),
+      zephjsInline(),
+      copy({
+        targets: [
+          { src: 'index.html', dest: '../../dist/' },
+          { src: 'global-style.css', dest: '../../dist/' },
+          { src: 'favicon.ico', dest: '../../dist/'}
+        ]
+      })
+    ]
   }
 ];
