e36d4d5c4af7bf7465990550e3a2cefc1ecc92eb
Shane Knudsen <shaneknu@gmail.com>
2020-04-26 21:35:08 -0400

    Converted form to object in app-search.js

--- src/frontend/search/app-search.js
+++ src/frontend/search/app-search.js
@@ -7,24 +7,43 @@ ZephComponents.define('app-search', () => {
 
   onCreate((element, content) => {
     const form = content.querySelector('form');
-    const intensityFieldset = content.querySelector('#intensity-fieldset');
-    intensityFieldset.addEventListener('click', () => {
-      search.next(getFormValue());
+    form.addEventListener('submit', evt => {
+      evt.preventDefault();
+      search(getFormData(form));
     });
 
-    function getFormValue() {
-      const intensityValues = Array.from(form.querySelectorAll('#intensity-fieldset input[type="checkbox"]'))
-        .filter(e => e.checked)
-        .map(e => e.value);
-      
-      const specialValues = Array.from(form.querySelectorAll('#special-fieldset input[type="checkbox"]'))
-        .filter(e => e.checked)
-        .map(e => e.value);
-
-      return {
-        intensity: intensityValues,
-        specialConditions: specialValues
-      };
-    }
   });
 });
+
+function getFormData(form) {
+  const formValues = {};
+
+  Array.from(new FormData(form))
+    .filter(formControl => formControl[1])
+    .forEach(formControl => {
+      let key = formControl[0],
+        value = formControl[1];
+
+      // clean up value a bit
+      if (isNaN(value)) {
+        value = value.toUpperCase();
+      } else {
+        value = +value;
+      }
+
+      // Check if key is an array name
+      if (key.charAt(key.length - 1) == ']') {
+        // strip the [] characters from the key
+        key = key.substr(0, key.length - 2);
+        if (formValues[key]) {
+          formValues[key].push(value);
+        } else {
+          formValues[key] = [ value ];
+        }
+      } else {
+        formValues[key] = value;
+      }
+    });
+
+  return formValues;
+}
--- src/frontend/services/searchSvc.js
+++ src/frontend/services/searchSvc.js
@@ -1,9 +1,10 @@
 import { BACKEND_HOST } from '../environments/environment.dev.js';
 import { hurricanes } from './eventSvc.js';
 
-function search(searchObj) {
-  const result = fetch(`${BACKEND_HOST}/query/search`, searchObj);
-  hurricanes.next(result);
+function search(searchData) {
+  // const result = fetch(`${BACKEND_HOST}/query/search`, searchData);
+  // hurricanes.next(result);
+  console.log(searchData);
 }
 
 export { search };
