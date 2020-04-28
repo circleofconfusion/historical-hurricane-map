1d37378a2425833cd271fcb14d3460768670165f
Shane Knudsen <shaneknu@gmail.com>
2020-04-27 23:59:47 -0400

    Updated search service to send json to subject eventSvc.hurricane.

--- src/frontend/services/searchSvc.js
+++ src/frontend/services/searchSvc.js
@@ -1,13 +1,27 @@
 import { BACKEND_HOST } from '../environments/environment.dev.js';
 import { hurricanes } from './eventSvc.js';
+import { of } from 'rxjs';
 import { fromFetch } from 'rxjs/fetch';
+import { switchMap, catchError } from 'rxjs/operators';
 
 function search(searchData) {
   // TODO: escape ampersands?
   const queryString = searchData.map(d => d.join('=')).join('&');
-  fromFetch(`${BACKEND_HOST}/search?${queryString}`).subscribe(result => {
-    hurricanes.next(result);
-  });
+  fromFetch(`${BACKEND_HOST}/search?${queryString}`)
+    .pipe(
+      switchMap(response => {
+        if (response.ok) return response.json();
+        else return of({ error: true, message: `Error ${response.status}`});
+      }),
+      catchError(error => {
+        console.error(error);
+        return of({ error: true, message: error.message });
+      })
+    )
+    .subscribe({
+      next: response => hurricanes.next(response),
+      complete: () => console.log('done')
+    });
 }
 
 export { search };
